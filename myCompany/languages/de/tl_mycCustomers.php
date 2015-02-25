95daf85e1720567261b665be8feb258e60f89f9a
Roman Wollenburg <roman@slash-works.de>
2015-02-25 16:44:36 +0100

    translations added + small fixes

---
+++ myCompany/languages/de/tl_mycCustomers.php
@@ -0,0 +1,20 @@
+<?php
+ /**
+ *
+ *          _           _                       _
+ *         | |         | |                     | |
+ *      ___| | __ _ ___| |____      _____  _ __| | _____
+ *     / __| |/ _` / __| '_ \ \ /\ / / _ \| '__| |/ / __|
+ *     \__ \ | (_| \__ \ | | \ V  V / (_) | |  |   <\__ \
+ *     |___/_|\__,_|___/_| |_|\_/\_/ \___/|_|  |_|\_\___/
+ *                                        web development
+ *
+ *     http://www.slash-works.de </> hallo@slash-works.de
+ *
+ *
+ * @author      rwollenburg
+ * @copyright   rwollenburg@slashworks
+ * @since       25.02.15 15:19
+ * @package     Core
+ *
+ */
